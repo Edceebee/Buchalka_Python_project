570b1a1160be1e7c898bdc2db6e68f465995adbd
Chinonso Edozie <edceebee@gmail.com>
2021-03-29 19:52:26 +0100

    contd practice

--- challenge.py
+++ challenge.py
@@ -5,19 +5,22 @@
 # Yo can either enter a text from the keyboard or
 # initialize a string variable with the string
 
-
-text = "My name is Star Boy"
-vowels = ('a', 'e', 'i', 'o', 'u')
-
-new_text = set(text).difference(vowels)
-print(new_text)
-
-final_text = sorted(new_text)
-print(final_text)
-
-# # for letters in text:
-# for l in text:
-#     if vowels in text:
-#         text = text.replace(l, "")
 #
-# print(text)
+# text = "My name is Star Boy"
+# vowels = ('a', 'e', 'i', 'o', 'u')
+#
+# new_text = set(text).difference(vowels)
+# print(new_text)
+#
+# final_text = sorted(new_text)
+# print(final_text)
+
+
+text = input("enter name\n")
+vowels = ('a', 'e', 'i', 'o', 'u')
+for x in text:
+    if x in vowels:
+        text = text.replace(x, "_")
+
+    # Print string without vowels
+print(text)
---
+++ file_io.py
@@ -0,0 +1,5 @@
+jabber = open('c:\users\userchip\desktop\buchalkaio.txt', 'r')
+for line in jabber:
+    print(line)
+
+jabber.close()
